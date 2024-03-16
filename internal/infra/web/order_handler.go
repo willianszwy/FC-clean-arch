53deafbb7043f3999f54f412bf303f480d095b0f
Willians Oliveira <willianszwy@gmail.com>
2024-03-16 17:15:14 -0400

    feat: add list order handler

--- internal/infra/web/order_handler.go
+++ internal/infra/web/order_handler.go
@@ -47,3 +47,17 @@ func (h *WebOrderHandler) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func (h *WebOrderHandler) List(w http.ResponseWriter, r *http.Request) {
+	listOrdersUseCase := usecase.NewListOrdersUseCase(h.OrderRepository)
+	output, err := listOrdersUseCase.Execute()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
